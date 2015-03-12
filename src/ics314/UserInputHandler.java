cfb13121ecd5e19e9f5fcc4aa6b1e1fa7ce409a7
kjcarroll3 <carroll3@hawaii.edu>
2015-03-11 19:38:44 -1000

    Added user input

--- src/ics314/UserInputHandler.java
+++ src/ics314/UserInputHandler.java
@@ -4,13 +4,17 @@ import java.util.Scanner;
 
 public class UserInputHandler {
 	private Scanner scan;
-
+	private String location;
+	private String summary;
+	private int priority;
+	private String classification;
+	private String dtstart;
+	private String dtend;
+	private String tmz;
+	
 	public static void main(String[] args) {
 		UserInputHandler userInputHandler = new UserInputHandler();
 		userInputHandler.mainLoop();
-		System.out.println("Hello world");
-		System.out.println("test");
-
 	}
 	
 	public UserInputHandler(){
@@ -34,7 +38,8 @@ public class UserInputHandler {
 	private void printMainOptions(){
 		String s = "1) Create a new Event\n"
 				+ "2) Print OutputFile\n"
-				+ "3) Exit Program\n";
+				+ "3) Test print\n"
+				+ "4) Exit Program\n";
 		System.out.println(s);
 	}
 	
@@ -44,12 +49,56 @@ public class UserInputHandler {
 	}
 	
 	private void processUserInput(Integer option){
+		String startDate = "";
+		String startTime = "";
+		String endDate = "";
+		String endTime = "";
+		
 		if(option.equals(1)){
-			System.out.println("Create New Event Menu");
+			System.out.println("--Enter new event information--");
+			scan.nextLine(); //extra newline somewhere that was messing up input
+			
+			System.out.print("Start Date(mmddyyyy): ");
+			startDate = scan.nextLine();
+			System.out.print("Start Time(24hr, 4 digits) ####: ");    //8 AM = 0800
+			startTime = scan.nextLine();
+			System.out.print("End Date(mmddyyyy): ");
+			endDate = scan.nextLine();
+			System.out.print("End Time(24hr, 4 digits) ####: ");    
+			endTime = scan.nextLine();
+			dtstart = (startDate.substring(4, 8) + startDate.substring(0,2) + startDate.substring(2,4) + "T" + startTime + "00Z");
+			dtend = (endDate.substring(4, 8) + endDate.substring(0,2) + endDate.substring(2,4) + "T" + endTime + "00Z");
+
+			System.out.print("Location: ");
+			location = scan.nextLine();
+			System.out.print("Summary: ");
+			summary = scan.nextLine();
+			System.out.print("Priority: ");
+			priority = scan.nextInt();
+			System.out.print("Classification: ");
+			classification = scan.next().toUpperCase();
+			
+			System.out.println("[1] Pacific/Honolulu   [2] Not implemented");
+			System.out.print("Select timezone: ");
+			Integer timeZone = scan.nextInt();
+			if (timeZone.equals(1)) {
+				tmz = "Pacific/Honolulu:";
+			}
 		}
+		
 		else if(option.equals(2)){
 			System.out.println("Export Output File Menu");
 		}
+		
+		else if(option.equals(3)){
+			System.out.println("DTSTART;TZID=" + tmz + dtstart);
+			System.out.println("DTEND;TZID=" + tmz + dtend);
+			System.out.println("LOCATION:" + location);
+			System.out.println("SUMMARY:" + summary);
+			System.out.println("PRIORITY:" + priority);
+			System.out.println("CLASS:" + classification);
+		}
+		
 		else{
 			System.out.println("Exiting Program");
 			System.exit(0);
