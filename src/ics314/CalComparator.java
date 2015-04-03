05cf69aa5197be11b32d9b6d2adf9d260605b0c7
kjcarroll3 <carroll3@hawaii.edu>
2015-04-02 21:59:35 -1000

    Comparator and Updated TesterClass(sorting)

--- src/ics314/CalComparator.java
+++ src/ics314/CalComparator.java
@@ -1,15 +1,12 @@
 package ics314;
 
-import java.util.Calendar;
 import java.util.Comparator;
 
 public class CalComparator implements Comparator<ICSEvent>{
 
-
 	@Override
 	public int compare(ICSEvent o1, ICSEvent o2) {
-		// TODO Auto-generated method stub
-		return 0;
+		return o1.start.compareTo(o2.start);
 	}
 	
 }
--- src/ics314/TesterClass.java
+++ src/ics314/TesterClass.java
@@ -8,15 +8,25 @@ import java.util.List;
 public class TesterClass {
 
 	public static void main(String[] args) {
-		Calendar calendar = new GregorianCalendar(2015, 3, 2, 3, 12 ,12);
-		Calendar calendar2 = new GregorianCalendar(2015, 3, 2, 5, 12 ,12);
-		Calendar calendar3 = new GregorianCalendar(2015, 3, 2, 6, 12 ,12);
-		Calendar calendar4 = new GregorianCalendar(2015, 3, 2, 8, 12 ,12);
-		ICSEvent f = new ICSEvent(calendar, calendar2);
-		ICSEvent g = new ICSEvent(calendar3, calendar4);
+		CalComparator comparator = new CalComparator();
+		Calendar calendarStart1 = new GregorianCalendar(2015, 3, 1, 3, 12 ,12); //Apr 1
+		Calendar calendarEnd1 = new GregorianCalendar(2015, 3, 1, 5, 12 ,12);
+		Calendar calendarStart2 = new GregorianCalendar(2015, 3, 2, 6, 12 ,12); //Apr 2
+		Calendar calendarEnd2 = new GregorianCalendar(2015, 3, 2, 8, 12 ,12);
+		Calendar calendarStart3 = new GregorianCalendar(2015, 3, 3, 3, 12 ,12); //Apr 3
+		Calendar calendarEnd3 = new GregorianCalendar(2015, 3, 3, 5, 12 ,12);
+		Calendar calendarStart4 = new GregorianCalendar(2015, 3, 4, 6, 12 ,12); //Apr 4
+		Calendar calendarEnd4 = new GregorianCalendar(2015, 3, 4, 8, 12 ,12);
+		ICSEvent f = new ICSEvent(calendarStart1, calendarEnd1); //Apr 1
+		ICSEvent g = new ICSEvent(calendarStart2, calendarEnd2); //Apr 2
+		ICSEvent h = new ICSEvent(calendarStart3, calendarEnd3); //Apr 3
+		ICSEvent i = new ICSEvent(calendarStart4, calendarEnd4); //Apr 4
 		List<ICSEvent> events = new ArrayList<ICSEvent>();
-		events.add(g);
-		events.add(f);
+		events.add(g);  //Apr 2
+		events.add(f);  //Apr 1
+		events.add(i); // Apr 4
+		events.add(h); // Apr 3
+		events.sort(comparator);
 		System.out.println(events);
 	}
 
