139c7ba4dcf812b46479fd693c26d8b91cf61ab7
Nathan Kanekuni <nakaneku@gmail.com>
2015-04-15 09:01:45 -1000

    Added class that gets filenames for image files

---
+++ src/ics314/ImageFileNameObtainer.java
@@ -0,0 +1,34 @@
+package ics314;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
+
+public class ImageFileNameObtainer {
+	@SuppressWarnings("all")
+	public static void main(String[] args){
+		List<String> imageFileNames = getListOfImageFileNames("./images");
+		System.out.println(imageFileNames);
+		
+		
+		
+	}
+	
+	public static List<String> getListOfImageFileNames(String directory){
+		File f = new File("./" + directory);
+		String[] fileNames = f.list();
+		
+		List<String> imageFileNames = new ArrayList<String>(10);
+		String r = "image_.*\\.jpg";
+		Pattern p = Pattern.compile(r);
+		
+		for(String s : fileNames){
+			if(p.matcher(s).matches()){
+				imageFileNames.add(s);
+			}
+		}
+		return imageFileNames;
+	}
+}
