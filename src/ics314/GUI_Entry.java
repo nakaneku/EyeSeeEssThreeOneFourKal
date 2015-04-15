9c0b7bb8f43844a3048b9d70f04dd1ca863551fc
Nathan Kanekuni <nakaneku@gmail.com>
2015-04-14 23:24:34 -1000

    Added dropdown box support

--- src/ics314/GUI_Entry.java
+++ src/ics314/GUI_Entry.java
@@ -2,11 +2,12 @@ package ics314;
 
 
 
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Vector;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -18,6 +19,8 @@ public class GUI_Entry {
 	JFrame frame;
 	JTextField eventNameTextField;
 	JLabel eventNameLabel;
+	JComboBox<String> timeZone;
+	JLabel timeZoneLabel;
 	public static void main(String [] args){
 		
 		final class GuiRunner implements Runnable {
@@ -42,25 +45,39 @@ public class GUI_Entry {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		button = new JButton("Submit");
-		eventNameTextField = new JTextField(20);
-		eventNameLabel = new JLabel("Event Name");
-		
-		eventNameLabel.setBounds(10,10, 130,25);
-		eventNameTextField.setBounds(170,10, 130, 25);
 		button.setBounds(400, 300, 200, 40);
 		button.addActionListener(new SubmitButtonHandle());
-		
-		frame.add(eventNameTextField);
-		frame.add(eventNameLabel);
 		frame.add(button);
 		
+		
+		eventNameTextField = new JTextField(20);
+		eventNameTextField.setBounds(170,10, 130, 25);
+		frame.add(eventNameTextField);
+		
+		eventNameLabel = new JLabel("Event Name");
+		eventNameLabel.setBounds(10,10, 130,25);
+		frame.add(eventNameLabel);
+		
+		timeZoneLabel = new JLabel("TimeZone");
+		timeZoneLabel.setBounds(10, 50, 130, 25);
+		
+		frame.add(timeZoneLabel);
+		initTimeZone();
+		frame.add(timeZone);
 
-		
-		
-		
 		//frame.pack();
 		frame.setVisible(true);
 		
+	}
+	
+	private void initTimeZone(){
+		
+		Vector<String> timeZoneList = new Vector<String>();
+		timeZoneList.add("Pacific/Honolulu");
+		timeZoneList.add("Pacific/Cali");
+		timeZoneList.add("Tester");
+		timeZone = new JComboBox<String>(timeZoneList);
+		timeZone.setBounds(170,50,155,25);
 	}
 	
 	private class SubmitButtonHandle implements ActionListener{
@@ -69,7 +86,7 @@ public class GUI_Entry {
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			System.out.println(eventNameTextField.getText());
-			
+			System.out.println(timeZone.getSelectedItem());
 		}
 		
 	}
